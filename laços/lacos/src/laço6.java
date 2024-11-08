3f3d627419df4b0cfb597569d8f314555d1619c9
camilopower <camilo.richter@hotmail.com>
2024-11-08 18:19:49 -0300

    Add files via upload

---
+++ laços/lacos/src/laço6.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class laço6 {
+
+
+    public static void main(String[] args) {
+        int i = 0;
+
+        while(i < 10 ) {
+            i++;
+            if (i == 5) break;
+            System.out.println("i =" + i);
+
+        }
+        System.out.println("fora do while");
+    }
+}
---
+++ laços/lacos/src/laços5.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class laços5{
+    public static void main(String[] args) {
+
+        ArrayList<String> nomes = new ArrayList<String>();
+        nomes.add("guilherme");
+        nomes.add("mario");
+        for(String nome : nomes) {
+            System.out.println(nome);
+
+
+
+        }
+        System.out.println(nomes);
+    }
+}
